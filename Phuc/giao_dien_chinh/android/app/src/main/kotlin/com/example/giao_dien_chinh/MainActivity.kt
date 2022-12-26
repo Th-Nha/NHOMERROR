d92311d68dd75ef1cfe9686ad7287cc400f513d0
HoangPhuc <0306201274@caothang.caothang.edu.vn>
2022-12-26 15:36:34 +0700

    choi don

--- Phuc/giao_dien_chinh/android/app/src/main/kotlin/com/example/giao_dien_chinh/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.giao_dien_chinh
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Phuc/thaydoiprofile/android/app/src/main/kotlin/com/example/thaydoiprofile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.thaydoiprofile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
